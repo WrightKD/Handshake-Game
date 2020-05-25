deb737c9c1a591ec8c56722b7de17f0b259339e6
Kenneth <kennethw@bbd.co.za>
2020-05-25 12:50:03 +0200

    remove extra methods from Manage

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -101,6 +101,7 @@ namespace Handshake.Controllers
             {
                 var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
                 var result = await _userManager.CreateAsync(user, model.Password);
+                
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
