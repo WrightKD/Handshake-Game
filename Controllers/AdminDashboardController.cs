7a6b50ce162ff6a32177356190528fc38df370f2
RIBAJACOB <jacobriba7@gmail.com>
2020-05-28 20:00:36 +0200

    added admin controller

---
+++ Controllers/AdminDashboardController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Handshake.Database;
+using Handshake.Models;
+using HandshakeGame.Controllers;
+using HandshakeGame.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApp.Models;
+using WebApp.Models.AccountViewModels;
+using Handshake.Services;
+
+namespace Handshake.Controllers
+{
+    
+    public class AdminDashboardController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        private readonly ILogger _logger;
+        private AdminService  _adminService;
+
+        public AdminDashboardController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<ApplicationRole> roleManager, ILogger<AccountController> logger,AdminService  adminService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _roleManager = roleManager;
+            _adminService = adminService;
+        }
+
+        public async Task<IActionResult> ListUsers()
+        {
+            ViewBag.Users = await _adminService.GetUsers();
+            _logger.LogInformation(" Admin is Displaying All Users ");
+
+            return View("~/Views/Manage/AdminDashboard.cshtml");
+        }
+
+    }
+
+}
