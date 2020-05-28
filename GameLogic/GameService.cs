4d77ca7e9a610b64913f113754b29cc91c6fe90d
Marcelle <marcelleb@bbd.co.za>
2020-05-28 23:46:37 +0200

    UI updated

--- GameLogic/GameService.cs
+++ GameLogic/GameService.cs
@@ -26,10 +26,10 @@ namespace Handshake.GameLogic
         private Random infectionRoll;
         private double valForRandomDouble;
         private double nPCInfectedChance; //affected by regional stats
-        private double nPCRefreshTime; 
+        private double nPCRefreshTime;
 
-    public GameService() 
-        { 
+        public GameService()
+        {
             player = new Player();
             handShakePoints = 1;
             NPCs = new List<NPC>();
@@ -65,7 +65,7 @@ namespace Handshake.GameLogic
         {
             for (int i = 0; i < numberToSpawn; i++)
             {
-                NPCs.Add(new NPC(i, nPCInfectedChance, "John", "A simple man"));
+                NPCs.Add(new NPC(i, nPCInfectedChance, GetNPCName(i), GetNPCTraits(i)));
             }
         }
 
@@ -131,25 +131,241 @@ namespace Handshake.GameLogic
             Shop shop = shops.Find(x => x.ID == shopId);
             if (shop.SanitiserCount > 0 && player.Gold > shop.SanitiserCost)
             {
-                shop.SanitiserCount --;
+                shop.SanitiserCount--;
                 player.SanitiserCount++;
                 player.Gold -= shop.SanitiserCost;
             }
             return shop;
         }
-        
+
         private void TEMPGenerateShop(int numberToSpawn)
         {
             shops = new List<Shop>();
             for (int i = 0; i < numberToSpawn; i++)
             {
-                shops.Add(new Shop(i,sanitiserCount, sanitiserCost, maskCount, maskCost));
+                shops.Add(new Shop(i, sanitiserCount, sanitiserCost, maskCount, maskCost));
             }
         }
 
         private double GetRandomDouble()
         {
             return spawnRoll.NextDouble() * (2 * valForRandomDouble) - valForRandomDouble;
+        }
+        private string GetNPCName(int id)
+        {
+            List<string> names = new List<string>()
+            {
+                "Shoaib Herbert",
+                "Aiyla Tran",
+                "Archibald Akhtar",
+                "Elsie-Mae Greene",
+                "Missy Mcleod",
+                "Sheila Burrows",
+                "Youssef Durham",
+                "Eduardo Knox",
+                "Austen Bryant",
+                "Suleman Goodman",
+                "Callen Sharples",
+                "Sumayyah Allison",
+                "Malaikah Langley",
+                "Patrik Koch",
+                "Rishi Buxton",
+                "Hallam Sumner",
+                "Zackary Wade",
+                "Montague Robles",
+                "Frank Mack",
+                "Johan Handley",
+                "Nel Chadwick",
+                "Xander Patel",
+                "Lubna Nunez",
+                "Kerry Macleod",
+                "Gina Guevara",
+                "Phoebe Mcgrath",
+                "Simrah Wise",
+                "Ali East",
+                "Alina Cotton",
+                "Todd Wagner",
+                "Clark Suarez",
+                "Esther Mooney",
+                "Ruby-Rose Montoya",
+                "Jagdeep Connor",
+                "Ida Glass",
+                "Riley-James Stark",
+                "Tabitha Higgins",
+                "Joyce Dougherty",
+                "Efe Bruce",
+                "Kayleigh Kearns",
+                "Shamima Howell",
+                "Vanesa Lake",
+                "Caspian Clements",
+                "Macey Daniel",
+                "Elizabeth Peters",
+                "Kathleen Witt",
+                "Natalya Power",
+                "Faizaan Mustafa",
+                "Kamron Rice",
+                "Callum Gardiner",
+                "Nayan Sparks",
+                "Nile Hamer",
+                "Kiana Hirst",
+                "Kaleem Dunn",
+                "Dewey Tucker",
+                "Alanah Hickman",
+                "Zaina Kidd",
+                "Kerri Fellows",
+                "Maxine Ellison",
+                "Abdur-Rahman Worthington",
+                "Darrel Curtis",
+                "Rosa Beaumont",
+                "Jordon Marsh",
+                "Corban Finnegan",
+                "Ryley Wardle",
+                "Ada Oneal",
+                "Codie Senior",
+                "Blossom Stuart",
+                "Jamel Mcdonnell",
+                "Wilson Blake",
+                "Koa Lim",
+                "Darius Mays",
+                "Dion Walker",
+                "Darin Joyner",
+                "Santiago Charles",
+                "Salma Avalos",
+                "Jaydn Calvert",
+                "Gurpreet Summers",
+                "Nansi Hanson",
+                "Danika Macias",
+                "Ignacy Watkins",
+                "Juliet Sierra",
+                "Cain Chavez",
+                "Patsy Mendoza",
+                "Anum Prosser",
+                "Amelia-Lily Evans",
+                "Cristian Chester",
+                "Abubakar Maguire",
+                "Grover Logan",
+                "Rohit Bass",
+                "Hajrah Villarreal",
+                "Anas Neal",
+                "Carolyn Arroyo",
+                "Javan Hernandez",
+                "Kaan Ballard",
+                "Kayan Bonner",
+                "Ariel Ferreira",
+                "Connar Bowes",
+                "Brax Burris",
+                "Morwenna Mckee"
+            };
+
+            return names[id];
+        }
+
+        private string GetNPCTraits(int id)
+        {
+            List<string> traits = new List<string>()
+            {
+                "devious, mysterious and decisive",
+                "easy-going, generous and cheerful",
+                "courageous, overbearing and ambitious",
+                "brilliant, dependable and spontaneous",
+                "selfish, violent and foolish",
+                "sly, materialistic and restless",
+                "depressed, wise and rude",
+                "independent, judgemental and wise",
+                "rude, eccentric and stubborn",
+                "secretive, lonely and ambitious",
+                "uninhibited, violent and caring",
+                "dishonest, charming and uninhibited",
+                "fun-loving, imaginative and shy",
+                "cruel, courageous and anxious",
+                "daring, fun-loving and industrious",
+                "wise, caring and courageous",
+                "naive, selfish and depressed",
+                "materialistic, foolish and conscientious",
+                "materialistic, easy-going and inspirational",
+                "materialistic, daring and imaginative",
+                "rebellious, uninhibited and idealistic",
+                "cheerful, compassionate and materialistic",
+                "optimistic, carefree and idealistic",
+                "naive, kindly and cheerful",
+                "coarse, nurturing and courageous",
+                "optimistic, restless and reckless",
+                "inspirational, lively and shy",
+                "conscientious, optimistic and conceited",
+                "caring, arrogant and decisive",
+                "mysterious, restless and judgemental",
+                "fearless, charming and idealistic",
+                "depressed, restless and bold",
+                "nurturing, caring and devious",
+                "charming, caring and eccentric",
+                "considerate, cheerful and shy",
+                "lonely, nurturing and eccentric",
+                "industrious, cruel and naive",
+                "cowardly, lively and helpful",
+                "conscientious, rebellious and materialistic",
+                "cheerful, secretive and stubborn",
+                "cowardly, coarse and lively",
+                "conscientious, reckless and idealistic",
+                "brilliant, rude and caring",
+                "rude, mysterious and sensitive",
+                "ambitious, cowardly and rebellious",
+                "naive, restless and fun-loving",
+                "lively, fun-loving and bold",
+                "aggressive, compassionate and idealistic",
+                "inspirational, devious and idealistic",
+                "forceful, overbearing and devious",
+                "foolish, industrious and selfish",
+                "spontaneous, wise and aggressive",
+                "secretive, spontaneous and idealistic",
+                "compassionate, courageous and manipulative",
+                "spontaneous, independent and manipulative",
+                "rude, bold and anxious",
+                "sly, bold and cowardly",
+                "lonely, rude and idealistic",
+                "restless, ambitious and caring",
+                "secretive, cheerful and aggressive",
+                "unkind, wise and naive",
+                "industrious, ambitious and selfish",
+                "cheerful, manipulative and stubborn",
+                "spontaneous, selfish and shy",
+                "inspirational, lonely and cowardly",
+                "inspirational, forceful and dishonest",
+                "conscientious, timid and compassionate",
+                "forceful, courageous and restless",
+                "charming, aggressive and cheerful",
+                "mysterious, rebellious and shy",
+                "spontaneous, naive and aggressive",
+                "optimistic, secretive and industrious",
+                "optimistic, lively and selfish",
+                "dishonest, conscientious and naive",
+                "dependable, secretive and decisive",
+                "charming, rude and brilliant",
+                "conceited, lively and coarse",
+                "shy, lonely and naive",
+                "unkind, manipulative and compassionate",
+                "aggressive, fearless and violent",
+                "imaginative, helpful and charming",
+                "overbearing, stubborn and violent",
+                "wise, judgemental and sensitive",
+                "optimistic, inspirational and generous",
+                "wise, rude and carefree",
+                "brilliant, helpful and daring",
+                "conscientious, fearless and foolish",
+                "foolish, dependable and naive",
+                "reckless, cowardly and compassionate",
+                "dependable, spontaneous and brilliant",
+                "sly, naive and idealistic",
+                "eccentric, rude and aggressive",
+                "foolish, bold and independent",
+                "generous, overbearing and uninhibited",
+                "bold, independent and spontaneous",
+                "easy-going, materialistic and dishonest",
+                "forceful, lively and cheerful",
+                "arrogant, violent and fun-loving",
+                "independent, mysterious and reckless",
+                "courageous, conscientious and aggressive"
+            };
+            return traits[id];
         }
     }
 }
